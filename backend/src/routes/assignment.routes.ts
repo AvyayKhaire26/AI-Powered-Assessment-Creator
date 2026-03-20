bedc88059ba1d5742def76a5f7f21a0e5b77dd73
AvyayKhaire26 <avyay26@gmail.com>
2026-03-20 12:08:45 +0530

    updated routes

--- backend/src/routes/assignment.routes.ts
+++ backend/src/routes/assignment.routes.ts
@@ -4,8 +4,17 @@ import { AssignmentService } from "../services/assignment.service";
 import { AssignmentRepository } from "../repositories/assignment.repository";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { handleUpload } from "../middlewares/upload.handler";
+import { rateLimitMiddleware } from "../middlewares/rateLimit.middleware";
+import { validate } from "../middlewares/validate.middleware";
+import { CreateAssignmentSchema } from "../validators/assignment.validator";
 
 const router = Router();
+
+const aiRateLimit = rateLimitMiddleware({
+  capacity: 5,
+  refillRate: 3,
+  windowSecs: 60,
+});
 
 // DI wiring
 const repository = new AssignmentRepository();
@@ -14,12 +23,11 @@ const controller = new AssignmentController(service);
 
 router.use(authMiddleware);
 
-router.post("/", (req, res, next) => controller.create(req, res, next));
+router.post("/", aiRateLimit, handleUpload, validate(CreateAssignmentSchema), (req, res, next) => controller.create(req, res, next));
 router.get("/", (req, res, next) => controller.getAll(req, res, next));
 router.get("/:id", (req, res, next) => controller.getById(req, res, next));
 router.delete("/:id", (req, res, next) => controller.delete(req, res, next));
-router.post("/:id/regenerate", (req, res, next) => controller.regenerate(req, res, next));
+router.post("/:id/regenerate", aiRateLimit, (req, res, next) => controller.regenerate(req, res, next));
 router.post("/:id/pdf", (req, res, next) => controller.requestPdf(req, res, next));
-router.post("/", handleUpload, (req, res, next) => controller.create(req, res, next));
 
 export default router;
