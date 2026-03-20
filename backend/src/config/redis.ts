9e62a72bb99689faf73cd9a646e32112c882f6c5
AvyayKhaire26 <avyay26@gmail.com>
2026-03-20 11:30:37 +0530

    update redis

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,15 +1,16 @@
 import IORedis from "ioredis";
+import { env } from "./env";
 import { logger } from "../utils/logger";
 
-export const redisConnection = new IORedis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
+export const redisConnection = new IORedis(env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+  tls: env.REDIS_TLS ? {} : undefined,
+  lazyConnect: true,          // don't connect until first command
+  keepAlive: 10000,           // reduce reconnect chatter
+  connectTimeout: 10000,
+  retryStrategy: (times) => Math.min(times * 500, 5000),
 });
 
-redisConnection.on("connect", () => {
-  logger.info("Redis connected");
-});
-
-redisConnection.on("error", (err) => {
-  logger.error("Redis error", err);
-});
+redisConnection.on("connect", () => logger.info("Redis connected"));
+redisConnection.on("error", (err) => logger.error("Redis error", err));
