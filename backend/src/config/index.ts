579dc1f874ec085265c4141cfbb77e05bcab3801
AvyayKhaire26 <avyay26@gmail.com>
2026-03-20 11:30:20 +0530

    updated index for config

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,15 +1,16 @@
 import { connectDB } from "./db";
+import { redisConnection } from "./redis";
 import { assignmentQueue } from "./queue";
 import { logger } from "../utils/logger";
 
-export const initServices = async () => {
+export const initServices = async (): Promise<void> => {
   await connectDB();
 
-  logger.info("MongoDB initialized");
+  await redisConnection.ping();
+  logger.info("Redis initialized");
 
-  logger.info("Queue initialized");
-
-  assignmentQueue.on("error", (err) => {
-    logger.error("Queue error", err);
-  });
+  assignmentQueue.on("error", (err) => logger.error("Queue error", err));
+  logger.info("BullMQ queue initialized");
 };
+
+export { redisConnection, assignmentQueue };
