f31359ed13df453190ae8558b9d9222d66a33ad2
AvyayKhaire26 <avyay26@gmail.com>
2026-03-23 17:08:33 +0530

    updated route

--- backend/src/routes/assignment.routes.ts
+++ backend/src/routes/assignment.routes.ts
@@ -21,6 +21,8 @@ const repository = new AssignmentRepository();
 const service = new AssignmentService(repository);
 const controller = new AssignmentController(service);
 
+router.get("/:id/pdf", (req, res, next) => controller.requestPdf(req, res, next));
+
 router.use(authMiddleware);
 
 router.post("/", aiRateLimit, handleUpload, validate(CreateAssignmentSchema), (req, res, next) => controller.create(req, res, next));
@@ -28,6 +30,5 @@ router.get("/", (req, res, next) => controller.getAll(req, res, next));
 router.get("/:id", (req, res, next) => controller.getById(req, res, next));
 router.delete("/:id", (req, res, next) => controller.delete(req, res, next));
 router.post("/:id/regenerate", aiRateLimit, (req, res, next) => controller.regenerate(req, res, next));
-router.get("/:id/pdf", (req, res, next) => controller.requestPdf(req, res, next));
 
 export default router;
